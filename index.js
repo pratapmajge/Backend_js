9641c2f91b6b11e15cebecfef4bbbfee385e6d2c
pratapmajge <majgepratap1504@gmail.com>
2025-02-02 15:07:23 +0530

    index

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/twitter' , (req,res) => {
   res.send("Pratap")
 })
 
+app.get('/login' , (req , res) => {
+  res.send('<h1>Please Login</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
